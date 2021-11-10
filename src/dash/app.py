f27367d87f5d70277523be4567c07370b6d395f0
nullmaxwell <nullmaxwell@protonmail.com>
2021-11-10 15:04:57 -0600

    Modified basic app to include Bootstrap compatibility.

--- src/dash/app.py
+++ src/dash/app.py
@@ -4,10 +4,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
