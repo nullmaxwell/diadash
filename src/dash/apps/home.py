955e3bddc62649ffe9110a1612166e6f963f47b8
nullmaxwell <nullmaxwell@protonmail.com>
2021-12-03 10:48:13 -0600

    Removed test plot method.

--- src/dash/apps/home.py
+++ src/dash/apps/home.py
@@ -188,32 +188,6 @@ class mainContainer:
             return mainContainer.unknownView()
         pass
 
-    def getTestPlot() -> any:
-        """
-        Defines and returns the div containing a basic plot for the time being.
-
-        ## Parameters:
-        `plot` any:
-            The plot to insert into the main plot Div
-        """
-        df = pd.DataFrame(
-            {
-                "Fruit": [
-                    "Apples",
-                    "Oranges",
-                    "Bananas",
-                    "Apples",
-                    "Oranges",
-                    "Bananas",
-                ],
-                "Amount": [4, 1, 2, 2, 4, 5],
-                "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-            }
-        )
-
-        fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-        return dcc.Graph(id="example-graph", figure=fig)
-
     def getWeeklyView() -> any:
         """
         Defines and returns the weekly view of the plot.
